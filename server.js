8c4c39733acc49f5d18ec2c4147d42e3405a605b
Nova_Adi <95131750+Adittya-Gupta@users.noreply.github.com>
2023-08-20 12:38:38 +0530

    Added comments

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
-const express = require("express");
-const app = express();
-const server = require("http").Server(app);
-const io = require("socket.io")(server);
-const { v4: uuidV4 } = require("uuid");
-require("dotenv").config();
+const express = require("express"); // Importing express module
+const app = express(); // Creating express app
+const server = require("http").Server(app); // Creating server using express app
+const io = require("socket.io")(server); //importing the socket.io library and initializing it with the server object. This allows the server to listen for and handle WebSocket connections from clients.
+const { v4: uuidV4 } = require("uuid");  // importing the uuid library and assigning the v4 method of the library to a variable named uuidV4. This method generates a random UUID (Universally Unique Identifier) which can be used as a unique identifier for various purposes in the application.
+require("dotenv").config(); // For handling enviornment variables
 
-const bodyParser = require("body-parser");
+const bodyParser = require("body-parser"); //The body-parser library is used to parse incoming request bodies in middleware before the handlers, making it easier to handle data sent in HTTP requests.
 const ejs = require("ejs");
-const mongoose = require("mongoose");
-app.use("/", require("./routes/room.js"));
+const mongoose = require("mongoose"); // Mongoose for handling mongo db
+app.use("/", require("./routes/room.js")); // For '/' request send the room.js file
 
+//This library provides middleware for creating and managing user sessions in an Express.js application. It allows the server to store session data on the server-side and associate it with a unique session ID that is sent to the client in a cookie. This enables the server to maintain stateful information about the user across multiple requests.
 const session = require("express-session");
-const passport = require("passport");
-const passportLocalMongoose = require("passport-local-mongoose");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const findOrCreate = require("mongoose-findorcreate");
+const passport = require("passport"); //is a popular authentication middleware for Node.js applications. It provides a flexible and modular way to implement authentication strategies, such as local authentication with username and password, OAuth, and OpenID. 
+const passportLocalMongoose = require("passport-local-mongoose");// This library is a Mongoose plugin that simplifies building username and password login with Passport. It provides a set of methods to add username and password fields to a Mongoose schema, and to authenticate users against those fields. By using passport-local-mongoose, developers can easily add authentication to their Mongoose models.
+const GoogleStrategy = require("passport-google-oauth20").Strategy;// For handling google authentication
+const findOrCreate = require("mongoose-findorcreate"); // adding this method allows developers to search for a document in the database and create it if it doesn't exist, all in a single operation.
 
-app.use(express.static("public"));
+app.use(express.static("public")); // For using ejs as frontend
 app.set("view engine", "ejs");
 app.use(
   bodyParser.urlencoded({
@@ -24,6 +25,12 @@ app.use(
   })
 );
 
+/***
+ * The below code is setting up a session middleware using the express-session library. The session function takes an object with configuration options as its argument.
+ * The secret option is a string used to sign the session ID cookie. It should be a long, random string to prevent attackers from guessing the session ID and hijacking the session.
+ * The resave option determines whether the session should be saved back to the session store on every request, even if the session was not modified. Setting this to false can improve performance.
+ * The saveUninitialized option determines whether a session should be created for a new user who hasn't logged in yet. Setting this to false can help reduce server storage usage and comply with privacy laws.
+*/
 app.use(
   session({
     secret: "Our Little Secret",
@@ -32,11 +39,18 @@ app.use(
   })
 );
 
+
+/***
+ * The below code is setting up Passport.js middleware for authentication in an Express.js application.
+ * passport.initialize() initializes Passport and adds it as middleware to the Express.js application. This middleware is responsible for setting up Passport's authentication strategies and mounting Passport's middleware functions to the application's request object.
+ * passport.session() adds middleware to the Express.js application that will restore any existing login session from a user's cookie. This middleware is required for persistent login sessions, as it allows Passport to deserialize the user object from the session data stored on the server.
+ */
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect("mongodb://localhost:27017/userDB");
+mongoose.connect("mongodb://localhost:27017/userDB"); // Connecting to mongodb platform
 
+// Creating the schema for the MongoDb application
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
@@ -44,11 +58,18 @@ const userSchema = new mongoose.Schema({
   secret: String,
 });
 
+// Adding the two extra methods that we already imported
 userSchema.plugin(passportLocalMongoose);
 userSchema.plugin(findOrCreate);
 
-const User = new mongoose.model("User", userSchema);
+const User = new mongoose.model("User", userSchema); // Creating a MongoDb user
 
+/***
+ * The selected code is setting up Passport.js authentication strategies and serialization functions for a User model in an Express.js application.
+ * passport.use(User.createStrategy()) sets up a local authentication strategy for Passport using the User model. This strategy will be used to authenticate users based on their username and password.
+ * passport.serializeUser() is a function that is called by Passport to serialize the user object into the session. The User object is passed as the second argument to the done function, which is called when serialization is complete.
+ * passport.deserializeUser() is a function that is called by Passport to deserialize the user object from the session. The User object is passed as the second argument to the done function, which is called when deserialization is complete.
+ */
 passport.use(User.createStrategy());
 passport.serializeUser(function (User, done) {
   done(null, User);
@@ -58,7 +79,7 @@ passport.deserializeUser(function (User, done) {
   done(null, User);
 });
 
-let connections = [];
+let connections = []; // Making a list of all the connections
 
 //socket handels users joining/leaving and messaging
 io.on("connection", (socket) => {
@@ -75,10 +96,9 @@ io.on("connection", (socket) => {
   });
 });
 
-// let connections = [];
-
+// 
 io.on("connect", (socket) => {
-  connections.push(socket);
+  connections.push(socket); // Pushing the socket object once a client is connected
   console.log(`${socket.id} has connected`);
 
   socket.on("down", () => {
@@ -99,10 +119,10 @@ io.on("connect", (socket) => {
       }
     });
   });
-
+  // If some client gets disconnected
   socket.on("disconnect", (reason) => {
     console.log(`${socket.id} is disconnected`);
-    connections = connections.filter((con) => con.id !== socket.id);
+    connections = connections.filter((con) => con.id !== socket.id); // filter the connections array removing the socket object of the particular client
   });
 });
 
@@ -121,6 +141,8 @@ io.on("connect", (socket) => {
 //   }
 // ));
 
+// Authenticating users using the passport-google-oauth20 library.
+
 let vish;
 passport.use(
   new GoogleStrategy(
@@ -133,7 +155,8 @@ passport.use(
     },
     function (request, accessToken, refreshToken, profile, cb) {
       console.log(profile);
-      vish=profile.emails[0].value;
+      vish=profile.emails[0].value; 
+      // finds or creates a User document in the database using the findOrCreate method from the passport-local-mongoose library.
       User.findOrCreate(
         { username: profile.emails[0].value, googleId: profile.id },
         function (err, user) {
@@ -147,6 +170,7 @@ passport.use(
 
 let a;
 
+// a = uuidV4() generates a random UUID (Universally Unique Identifier) using the uuidV4 function from the uuid library and assigns it to a variable named a. This UUID can be used as a unique identifier for the client's session.
 app.get("/", function (req, res) {
   a = uuidV4();
   res.redirect(`/${a}`);
